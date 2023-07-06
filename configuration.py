c895886d56d9805c487783b455baf579966226fb
Lana Bolshakova <125307499+Lanushki@users.noreply.github.com>
2023-07-06 03:34:43 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://81bb1f67-875b-463d-977a-87c75203e6ad.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Билли",
+    "lastName": "Бутчер",
+    "address": "Подвал под Ломбардом",
+    "metroStation": 102,
+    "phone": "+79999999999",
+    "rentTime": 5,
+    "deliveryDate": "2023-06-16",
+    "comment": "Оставьте на ресепшене для Мясника",
+    "color": [
+        "BLACK"
+    ]
+}
